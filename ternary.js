307b46edc919fb8f150fdf3be5a3ba11de5660f7
memonyasin011 <memonyasin011@gmail.com>
2025-10-30 15:26:19 +0530

    ternary.js

---
+++ ternary.js
@@ -0,0 +1,29 @@
+let age = 20;
+
+if (age < 18) {
+    console.log("you are minor");
+}
+else{
+    console.log("you are adult");
+}
+///////////////////////////////////////////////////////////////////
+
+//ternary
+let age2=25;
+
+let a=(age2 > 18 ) ? "adult" : "minor";
+
+console.log(a)
+
+///////////////////////////////////////////////////////////////////
+/*
+let marks = 40;
+
+if (marks > 35) {
+    console.log("you are pass")
+}
+else {
+    console.log("you are fail")
+}
+
+*/
